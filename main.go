d65dd2ed512fa54cba491d078dc99347a2cae86d
Amogh <adesai@cloudera.com>
2021-09-25 14:04:31 +0530

    Initial implementation of link checks

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://amazon.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+	fmt.Println(link, "is responding to traffic & is ok!")
 }
